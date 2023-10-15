a245eb23312603d02303141a3e636839e3d1956f
Atharv140903 <atharvkurde2003@gmail.com>
2023-10-15 14:46:39 +0530

    next.config.js file fixed

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    images: {
-      loader: 'akamai',
-      path: '',
-    },
-    assetPrefix: './',
 }
 
 module.exports = nextConfig
